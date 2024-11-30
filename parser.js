fd7294ef36d732ed1f5531eb573181c2ccd2d037
Mythxcal <75327936+Mythxcal@users.noreply.github.com>
2024-11-30 16:14:37 +0000

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,9 +1,10 @@
-const messages = document.querySelectorAll("discord-message");
-Array.from(messages).forEach((messageEl) => {
-    const messageMarkupEl = messageEl.querySelector(".discord-message-markup")
-    const messageCompactEl = messageEl.querySelector(".discord-message-compact-indent")
+document.addEventListener("DOMContentLoaded", () => {
+        Array.from(messages).forEach((messageEl) => {
+            const messageMarkupEl = messageEl.querySelector(".discord-message-markup");
+            const messageCompactEl = messageEl.querySelector(".discord-message-compact-indent");
 
-    if (!messageMarkupEl.textContent && messageCompactEl.children.length == 0) {
-        messageEl.remove(); 
-    } 
-})
+            if (!messageMarkupEl.textContent && messageCompactEl.children.length == 0) {
+                messageEl.remove();
+            }
+        });
+    });
